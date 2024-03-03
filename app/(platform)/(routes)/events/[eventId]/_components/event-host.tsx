7c5b34f94874af3de3d5a2fb93f9cee412ec4c08
KaimuSeino <skim07a19yki@icloud.com>
2024-03-03 20:25:08 +0900

    イベント画面の主催者の詳細を全て見れるようにした

--- app/(platform)/(routes)/events/[eventId]/_components/event-host.tsx
+++ app/(platform)/(routes)/events/[eventId]/_components/event-host.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { Host } from "@prisma/client";
+import { useState } from "react";
 
 interface EventHostProps {
   host: Host
@@ -8,10 +13,12 @@ interface EventHostProps {
 const EventHost = ({
   host
 }: EventHostProps) => {
+  const [onDetail, setOnDetail] = useState(false);
+  const toggleDetail = () => setOnDetail((current) => !current);
   return (
     <div>
       <p className="font-medium">主催者</p>
-      <div className="flex flex-row items-center justify-start">
+      <div className="flex flex-row items-start justify-start mt-2">
         <Avatar className="h-10 w-10">
           <AvatarImage src={host.image || ""} />
         </Avatar>
@@ -19,9 +26,21 @@ const EventHost = ({
           <p className="font-semibold">
             {host.campany}
           </p>
-          <p className="text-sm font-light line-clamp-2">
+          <p className={cn(
+            "text-sm font-light",
+            !onDetail && "text-sm font-light line-clamp-2"
+          )}>
             {host.detail}
           </p>
+          <div className="flex flex-col items-center text-slate-500">
+            <Button
+              variant="ghost"
+              className="absolute pt-0 mt-0"
+              onClick={toggleDetail}
+            >
+              {!onDetail ? "詳細を見る" : "戻る"}
+            </Button>
+          </div>
         </div>
       </div>
     </div>
